163581e40cbd8c825fc2f2ee002be33f86b7f759
Crislayne Santos <crislaynesantos1603@gmail.com>
2024-06-24 15:28:05 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,65 @@
+const nome = "Crislayne" //string
+const idade = 17 // number
+const maiorDeIdade = true //boolean
+
+const frase = `Meu nome é ${nome}, tenho ${idade} anos`
+
+if (idade >= 18) {
+    console.log("Maior de idade")
+} else {
+    console.log("Menor de idade")
+}
+
+const multiplicacao = idade * 2
+console.log (multiplicacao)
+
+console.log(frase)
+
+1. //
+const adicao = 7 + 6
+console.log (adicao)
+
+2. //
+const somaedivisao = 45 + 11 + 6/ 3
+console.log (somaedivisao)
+
+3. //
+const divisao = 200/5
+console.log (divisao)
+
+4. //
+const celular = 100
+const descontoloja = 10/100
+const valort = celular * descontoloja
+console.log(celular - valort)
+
+
+5. //
+const salario = 1200
+const desconto = 25/100
+const valor = salario * desconto
+console.log(salario + valor)
+
+6. //
+const base = 5
+const altura = 6
+const area= base * altura
+console.log(area)
+
+7. //
+const baseparede = 7
+const alturaparede = 2
+const parcial = baseparede * alturaparede 
+const total = parcial
+const tinta = parcial / 2 
+console.log(tinta)
+
+8. // 
+const diasrodados = 10
+const diaria = 120
+const kmrodados = 450
+const valorkmrodados = 0.75
+const dias = diasrodados * diaria
+const kmstotal = kmrodados * valorkmrodados
+console.log(dias + kmstotal)
+
